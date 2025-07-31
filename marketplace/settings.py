1e53401c231c729264d75136cd9bda5df18af1d6
Bipin-Kumar-Thapa <bipinkumarthapa736@gmail.com>
2025-07-31 17:40:48 +0545

    DATABASE setting

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL')
-        )
+     'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')  # Corrected the typo here
+    )
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
 }
